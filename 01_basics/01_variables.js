b17f8d978a24ae001a42e21e3a4031e2215daa97
Swa-1721 <swapnilawad17@gmail.com>
2024-06-12 10:37:51 +0000

    learn about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accoutId = 32175;
+let accountEmail="Swapnil17@gmail.com";
+var accountPassword="123456";
+accountCity = "Aurangabad"; 
+let accountName;
+
+// accoutId=456; // not allowed
+/*
+prefer not to use var 
+beacuse of issue in block scope and functional scope
+*/
+console.log(accountEmail);
+console.table([accoutId,accountEmail,accountPassword,accountCity,accountName]);
